a977c1d750480e1a582cc0ce68690838f8ab188a
samyak2403 <samyakkamble6659@gmail.com>
2026-04-07 16:10:05 +0530

    Fix JVM build mismatch and update build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.samyak2403.flashlightmine"
         minSdk = 24
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
